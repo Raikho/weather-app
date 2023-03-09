7e187801b50bafcaca3534a3f64507820f85bd69
Mateen <zahidx297@yahoo.com>
2023-03-09 16:38:56 -0500

    add html plugin and css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     mode: 'development',
     entry: {
-        main: path.resolve(__dirname, 'src/index.js'),
+        bundle: path.resolve(__dirname, 'src/index.js'),
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ],
 }
