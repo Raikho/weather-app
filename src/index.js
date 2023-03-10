d6074e404f372fcc6a4bc431f7b4e4391d65964e
Mateen <zahidx297@yahoo.com>
2023-03-10 01:11:19 -0500

    add weather url generator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './style.css'
 
-console.log('Hello, World!');
+const weatherKey = 'bba6f56f2dcd34db5581285bb884ee36';
+const weatherBaseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+class UrlGen {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+        this.keyPairs = [];
+    }
+    addKey(key, value) {
+        for (let pair of this.keyPairs) {
+            if (pair.key === key) {
+                pair.value = value;
+                return;
+            }
+        }
+        this.keyPairs.push({key, value});
+    }
+    getUrl() {
+        let out = this.baseUrl;
+        for (let i = 0; i < this.keyPairs.length; i++) {
+            let key = this.keyPairs[i].key;
+            let value = this.keyPairs[i].value;
+            out += (i == 0) ? '?' : '&';
+            out = out + key + '=' + value;
+        }
+        return out;
+    }
+}
+
+let weatherUrl = new UrlGen(weatherBaseUrl);
+weatherUrl.addKey('q', 'Los Angeles');
+weatherUrl.addKey('appid', weatherKey);
+
+console.log(weatherUrl.getUrl(), weatherUrl);
+
+
