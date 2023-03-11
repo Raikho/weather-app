91d7bdbe83a2b666ea5d29aa6f8aad89cb79a616
Mateen <zahidx297@yahoo.com>
2023-03-11 02:38:12 -0500

    add search button and error handling

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const weatherKey = 'bba6f56f2dcd34db5581285bb884ee36';
 const weatherBaseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 // ========================== SETUP ===================================
+
 let weatherUrl = new UrlGen(weatherBaseUrl);
 weatherUrl.addKey('appid', weatherKey);
 let state = {
@@ -13,26 +14,36 @@ let state = {
     temp: null,
 };
 
-function setCity(name) {
-    state.city = name;
-    const cityNode = document.querySelector('.city');
-    cityNode.textContent = state.city;
+document.getElementById('search-button').addEventListener('click', async () => {
+    let cityName = document.getElementById('city-input').value;
+    console.log(`searching for "${cityName}"...`); // DEBUG
+    if (cityName) {
+        await queryCity(cityName);
+    }
+});
+
+async function queryCity(cityName) {
+    weatherUrl.addKey('q', cityName);
+    try {
+        let response = await fetch(weatherUrl.getUrl(), {mode: 'cors'});
+        if (!response.ok)
+            throw new Error(response.statusText);
+        let json = await response.json();
+        if (json.cod != '200')
+            throw new Error(json.cod);
+        updateWeatherState(json);
+    } catch(err) {
+        console.log('An error occured while fetching: ', err);
+    }
 }
 
-async function getData() {
-    let response = await fetch(weatherUrl.getUrl(), {mode: 'cors'});
-    let json = await response.json();
-    console.log('json: ', json);
-    setData(json);
-}
-
-function setData(data) {
+function updateWeatherState(data) {
     state.temp = Math.round((Number(data.main.temp) - 273) * 1.8 + 32);
     state.city = data.name;
-    writeData(data);
+    writeWeatherState();
 }
 
-function writeData(data) {
+function writeWeatherState() {
     const tempNode = document.querySelector('.temp');
     const cityNode = document.querySelector('.city');
 
@@ -41,9 +52,7 @@ function writeData(data) {
 }
 
 // =========================== INIT ===================================
-setCity('New York City');
-weatherUrl.addKey('q', state.city);
-const weatherData = getData();
+queryCity('Austin');
 
 
 
